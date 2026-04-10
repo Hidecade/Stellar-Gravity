4a370d113d26c62e9b39ea51e76ea9418b9babd0
Hidecade <kpianofamily@gmail.com>
2026-04-10 23:20:33 +0900

    fix

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@
 
     window.StellarConfig = {
         app: {
-            version: "2026.04.10.15"
+            version: "2026.04.10.16"
         },
         combo: {
             bonusStep: 0.2,         // 1コンボごとに加算されるスコア倍率
